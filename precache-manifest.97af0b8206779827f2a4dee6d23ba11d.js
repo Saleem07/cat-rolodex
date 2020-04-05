3672251f42214e7fd8e01194e0bf79598195da8a
Saleem07 <mohammadsaleem07@outlook.com>
2020-04-06 00:28:57 +0530

    Updates

---
+++ precache-manifest.97af0b8206779827f2a4dee6d23ba11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa21ad9fb0375b5852d8e3badc0125d",
+    "url": "/cat-rolodex/index.html"
+  },
+  {
+    "revision": "511cf3f426d1a8e5b2a4",
+    "url": "/cat-rolodex/static/css/main.1e26bd64.chunk.css"
+  },
+  {
+    "revision": "54bb4253cd44c959f9a3",
+    "url": "/cat-rolodex/static/js/2.b80b1286.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-rolodex/static/js/2.b80b1286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "511cf3f426d1a8e5b2a4",
+    "url": "/cat-rolodex/static/js/main.da3416fd.chunk.js"
+  },
+  {
+    "revision": "27b866f24b7a956697ad",
+    "url": "/cat-rolodex/static/js/runtime-main.e4aa876c.js"
+  }
+]);
